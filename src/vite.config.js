491a29055785674b59bbd8a2f78b61818bb7234e
Ruu5LP <116436229+Ruu5LP@users.noreply.github.com>
2025-02-18 00:49:13 +0900

    update

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,20 +1,20 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-
-function react() {
-    return undefined;
-}
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        host: "localhost", // または "0.0.0.0"
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            buildDirectory: 'public/build',
+            input: ["resources/css/app.css", "resources/js/app.tsx"],
+            buildDirectory: "public/build",
             refresh: true,
         }),
         react(),
     ],
     esbuild: {
-        jsx: 'automatic',
+        jsx: "automatic",
     },
 });
